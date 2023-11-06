264126cf8709a5c6deba3ed3d38116544f10bc17
A888008 <zvonimir.mesinger@atos.net>
2023-11-06 14:00:12 +0100

    Initial commit

---
+++ WeatherAppMVVM/WeatherAppMVVM/WeatherAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppMVVMApp.swift
+//  WeatherAppMVVM
+//
+//  Created by MacBookProAtos on 06.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
